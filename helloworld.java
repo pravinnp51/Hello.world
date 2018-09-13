45d5276f703a6ff46488b2d58b8176d4eb75651e
Pravin Adhikari <pravinadhikari51@gmail.com>
2018-09-13 23:33:45 +0000

    Went back to commit 2

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,6 @@
 // This program will print hello world!
 public class Main {
     public static void main(String[] args) {
-	// This line print hello world
         System.out.println("Hello, World!");
-	System.out.println("Hello, git!");
     }
 }
