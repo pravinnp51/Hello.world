f7286ed4022c159c65917fb9a84954fe1c26304b
Pravin Adhikari <pravinadhikari51@gmail.com>
2018-09-13 22:55:19 +0000

    Added hello git print line

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
        // This lines prints hello world
-	 System.out.println("Hello, World!");
+	System.out.println("Hello, World!");
+	System.out.println("Hello, git!");
     }
 }
